463f051c3fb0c61b2508c6b353eaff9b86c840b6
selfix <lezach33@hotmail.com>
2023-11-06 15:40:02 -0500

    01 done need to change somthing in ex03

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,17 +1,29 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <string>
 
 int main(int ac, char **av)
 {
-	std::string inputFile, outputFile, line;	
-	std::ifstream file(av[1]);
-
-	line.find(av[2], 0);
-	
-	while (std::getline(file, line))
-	{
-		std::cout << line << std::endl;
+	std::string line;
+	std::string name = av[1];
+	std::string old_word = av[2];
+	std::string new_word = av[3];
+	std::ifstream file;
+	std::ofstream outfile;
+	if (ac != 4){
+		std::cerr << "Need 4 arguments" << std::endl;
+		return 0;
 	}
+	file.open(name, std::ios::in);
+	outfile.open(name + ".replace", std::ios::out);
+	while (getline(file, line))
+	{
+		int start = line.find(old_word, 0);
+		while (start != std::string::npos){
+			line = line.substr(0, start) + new_word + line.substr(start + old_word.length(), line.length());
+			start = line.find(old_word, 0);
+		}
+		outfile << line + '\n';
+	}
+	file.close();
+	return 1;
 }
