4405d42d1feb19711083c80b0fd83b590fd42494
selfix123 <lezach33@hotmail.com>
2024-02-15 13:09:02 -0500

    cpp 08 done

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <stack>
 
 
-template<typename T>
-class MutantStack{
+#pragma once
 
+template <typename T>
+class MutantStack : std::stack<T>
+{
+private:
+
+public:
+	MutantStack():std::stack<T>(){};
+	MutantStack(const MutantStack &inst): std::stack<T>(inst){};
+	~MutantStack(){};
+	MutantStack& operator=(const MutantStack &rhs){
+		std::stack<T>::operator=(rhs);
+		return *this;
+	}
+	typedef typename std::stack<T>::container_type::const_iterator it;
+	it begin(){return (std::stack<T>::c.begin());};
+	it end() {return (std::stack<T>::c.end());};
 };
